66a98cd9946138fe20f4212e18714cedb64af5e8
hoodaddeveloper <mohammadkhaffas@gmail.com>
2023-11-20 14:58:02 +0100

    Ein einzelnes Zeichen abfragen

--- German-Tutorials/3-Weitere-Grundlagen.js
+++ German-Tutorials/3-Weitere-Grundlagen.js
@@ -1,9 +1,13 @@
 "Use Strict"
 
-let greeting = "--- Hallo Hoodad ---"
+let greeting = "--- Hallo Hoodad! ---"
 
 // Die Anzahl der zeichen in einem String
-console.log(greeting.length) // 20
+console.log(greeting.length) // 21
 
 // Ein einzelnes Zeichen abfragen
+console.log(greeting[4]) // H
 console.log(greeting.charAt(4)) // H
+
+// Wo findet sich ein Zeichen in einem String
+console.log(greeting.indexOf("!")) // H
