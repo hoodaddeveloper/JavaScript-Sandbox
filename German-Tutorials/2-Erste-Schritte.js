89b8c70303776def583ad2fde7dfcf388fdc17b7
hoodaddeveloper <mohammadkhaffas@gmail.com>
2023-11-20 11:17:45 +0100

    typeof

--- German-Tutorials/2-Erste-Schritte.js
+++ German-Tutorials/2-Erste-Schritte.js
@@ -6,12 +6,16 @@ console.log(bergüßung)
 
 // String
 let stringZahl = "50"
+console.log(typeof stringZahl) // Shows the type of the variable too
 console.log(stringZahl)
 
 // Zahlen
 let zahl = 100
+console.log(typeof zahl)
 console.log(zahl)
 
 // Booleans
 let positiv = true
 let negativ = false
+console.log(typeof positiv)
+console.log(positiv)
