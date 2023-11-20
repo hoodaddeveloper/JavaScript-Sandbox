b9dc59ebdc9d1957ea64738f8cc9616c2d3c886e
hoodaddeveloper <mohammadkhaffas@gmail.com>
2023-11-20 11:06:56 +0100

    string

--- German-Tutorials/2-Erste-Schritte.js
+++ German-Tutorials/2-Erste-Schritte.js
@@ -1,4 +1,17 @@
 // Abschnitt 2: Erste Schritte
 console.log("Hallo JavaScript")
-bergüßung = "Guten Morgen"
+
+let bergüßung = "Guten Morgen"
 console.log(bergüßung)
+
+// String
+let stringZahl = "50"
+console.log(stringZahl)
+
+// Zahlen
+let zahl = 100
+console.log(zahl)
+
+// Booleans
+let positiv = true
+let negativ = false
