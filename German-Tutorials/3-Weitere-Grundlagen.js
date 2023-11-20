b2c548c15a7e193f1c551b7aa237bf765efa5b4c
hoodaddeveloper <mohammadkhaffas@gmail.com>
2023-11-20 15:02:45 +0100

    Wo findet sich ein Zeichen in einem String

--- German-Tutorials/3-Weitere-Grundlagen.js
+++ German-Tutorials/3-Weitere-Grundlagen.js
@@ -10,4 +10,15 @@ console.log(greeting[4]) // H
 console.log(greeting.charAt(4)) // H
 
 // Wo findet sich ein Zeichen in einem String
-console.log(greeting.indexOf("!")) // H
+console.log(greeting.indexOf("!")) // 16
+console.log(greeting.indexOf("-", 10)) // 18
+
+// Großbuchstaben bzw. Kleinbuchstaben
+console.log(greeting.toUpperCase()) // --- HALLO HOODAD! ---
+console.log(greeting.toLowerCase()) // --- hallo hoodad! ---
+
+// Text ersetzen
+console.log(greeting.indexOf("-", 10)) // 18
+
+// Zeichen rechts und links vom String entfernen
+console.log(greeting.indexOf("-", 10)) // 18
