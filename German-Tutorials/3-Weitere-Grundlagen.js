dcd7672fb35103b158f8120fc5ae7827a6ba11b4
hoodaddeveloper <mohammadkhaffas@gmail.com>
2023-11-20 14:53:22 +0100

    Die Anzahl der zeichen in einem String

---
+++ German-Tutorials/3-Weitere-Grundlagen.js
@@ -0,0 +1,9 @@
+"Use Strict"
+
+let greeting = "--- Hallo Hoodad ---"
+
+// Die Anzahl der zeichen in einem String
+console.log(greeting.length) // 20
+
+// Ein einzelnes Zeichen abfragen
+console.log(greeting.charAt(4)) // H
