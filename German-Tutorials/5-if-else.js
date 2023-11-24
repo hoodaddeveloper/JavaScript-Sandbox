582851524a9c5a7d3d6b3e5bbc5fd2ecdec14a9e
hoodaddeveloper <mohammadkhaffas@gmail.com>
2023-11-24 10:26:05 +0100

    if else statements

--- German-Tutorials/5-if-else.js
+++ German-Tutorials/5-if-else.js
@@ -5,3 +5,20 @@ console.log(a) // true
 
 let b = "Hello" === "Hallo"
 console.log(b) // false
+
+let zahl1 = 5
+let zahl2 = 3
+
+if (zahl1 <= zahl2) {
+  console.log("z1 ist kleiner als z2")
+}
+
+/// /// /// /// /// ///
+
+students = 11
+
+if (students <= 10) {
+  console.log("weniger als 10 oder gleich 10")
+} else {
+  console.log("mehr als 10")
+}
