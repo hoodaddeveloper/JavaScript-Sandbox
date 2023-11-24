a8898cf3c708defbc3590dabb696748a04505f23
hoodaddeveloper <mohammadkhaffas@gmail.com>
2023-11-24 10:29:11 +0100

    .

--- German-Tutorials/5-if-else.js
+++ German-Tutorials/5-if-else.js
@@ -15,10 +15,12 @@ if (zahl1 <= zahl2) {
 
 /// /// /// /// /// ///
 
-students = 11
+students = 10
 
-if (students <= 10) {
-  console.log("weniger als 10 oder gleich 10")
+if (students < 10) {
+  console.log("weniger als 10")
+} else if (students = 10) {
+  console.log("gleich 10")
 } else {
   console.log("mehr als 10")
 }
