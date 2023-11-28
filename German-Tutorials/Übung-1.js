94bfaedd0cb46d3c9e8ecb43631e1d9f51f17a74
hoodaddeveloper <mohammadkhaffas@gmail.com>
2023-11-28 14:48:22 +0100

    commented answers

--- German-Tutorials/Übung-1.js
+++ German-Tutorials/Übung-1.js
@@ -10,27 +10,27 @@ function f3(x, y) { // x = 10, y = 20
   return x + y; // 30 + -10 = 20
 }
 
-function f2(x, y) {
+function f2(x, y) { // x = 10, y = 20
   console.log("F2")
-  let a = x;
-  let b = y;
-  let c = f3(a, b);
+  let a = x; // a = 10
+  let b = y; // b = 20
+  let c = f3(a, b); // c = 20
   console.log("F2 x=", x, " y=", y);
   console.log("F2 a=", a, " b=", b, " c=", c);
-  return c;
+  return c; // 20
 }
 
-function f1(x, y) {
+function f1(x, y) { // x = 10, y = 20
   console.log("F1")
-  let a = 2 * x;
-  let b = x + y;
-  let c = f2(a, b);
+  let a = 2 * x; // a = 20
+  let b = x + y; // b = 30
+  let c = f2(a, b); // c = 30
   console.log("F1 x=", x, " y=", y);
   console.log("F1 a=", a, " b=", b, " c=", c);
-  return c;
+  return c; // 30
 }
 
-function main() {
+function main() { // x = 10, y = 20
   console.log("Main")
   let x = 10;
   let y = 20;
