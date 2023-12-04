7ba4b03f50b321386d4fb7c3e2c36de51e2a22f6
hoodaddeveloper <mohammadkhaffas@gmail.com>
2023-12-04 15:25:35 +0100

    fixed the code

--- German-Tutorials/Übung-3.ts
+++ German-Tutorials/Übung-3.ts
@@ -20,9 +20,9 @@ for (let i: number = 0; i < 5; i++) {
 }
 console.log("Ende x = ", x);
 
-////////////////////////////
+///////////////////////////////////////////
 
-function f2(a: number, b: number): number {
+/* function f2(a: number, b: number): number {
   return a + b;
 }
 
@@ -39,4 +39,36 @@ function main(): void {
 console.log("Ergebnis von f1(x, y):", z);
 
 let result = f1(y, "30");
-console.log("Ergebnis von f1(y, 30):", result)
+console.log("Ergebnis von f1(y, 30):", result); */
+
+
+
+
+
+
+///////////////////////////////////////////
+
+
+function f2x(aa: number, bb: number): number {
+  return aa + bb;
+}
+
+function f1x(aa: number, bb: string): number {
+  // Hier sollte das Ergebnis von f2 zurückgegeben werden
+  return f2x(aa, parseInt(bb, 10)); // Die Zeichenkette 'b' muss in eine Zahl umgewandelt werden
+}
+
+function mainx(): void {
+  let xx: number = 10;
+  let yy: number = 20;
+
+  // Die Variable z muss innerhalb der main-Funktion deklariert werden
+  let zz: number = f1x(xx, yy.toString());
+  console.log("Ergebnis von f1(x, y):", zz);
+
+  let resultx = f1x(yy, "30");
+  console.log("Ergebnis von f1(y, 30):", resultx);
+}
+
+// main-Funktion aufrufen, um den Code auszuführen
+mainx();
