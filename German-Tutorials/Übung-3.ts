1e2944f735f367d49100018fb06b3c940d200bcd
hoodaddeveloper <mohammadkhaffas@gmail.com>
2023-12-04 15:20:13 +0100

    practicing typescript

--- German-Tutorials/Übung-3.ts
+++ German-Tutorials/Übung-3.ts
@@ -19,3 +19,24 @@ for (let i: number = 0; i < 5; i++) {
   }
 }
 console.log("Ende x = ", x);
+
+////////////////////////////
+
+function f2(a: number, b: number): number {
+  return a + b;
+}
+
+function f1(a: number, b: string): number {
+  f2(a, b);
+}
+
+function main(): void {
+  let x: number = 10;
+  let y: number = 20;
+  let z: number = f1(x, y);
+}
+
+console.log("Ergebnis von f1(x, y):", z);
+
+let result = f1(y, "30");
+console.log("Ergebnis von f1(y, 30):", result)
