70b5e67f1da0d16919c0a698b4f718b6f8ba223b
hoodaddeveloper <mohammadkhaffas@gmail.com>
2024-06-30 14:19:31 +0200

    Round down the Newton temperature number

--- Bootcamp-Tutorials/tutorial-26.js
+++ Bootcamp-Tutorials/tutorial-26.js
@@ -1,16 +1,19 @@
 /// Weather converter ///
 
 // Kelvin temperature
-const kelvin = 0;
+const kelvin = 100;
 // Convert Kelvin to Celsius
 const celsius = kelvin - 273;
 // Convert Celsius to Fahrenheit
 let fahrenheit = celsius * (9 / 5) + 32;
-// Round down the Fahrenheit number
+// Round down the Fahrenheit temperature number
 fahrenheit = Math.floor(fahrenheit);
-// Convert Newton to Celsius
+// Convert Celsius to Newton
 let newton = celsius * (33 / 100);
+// Round down the Newton temperature number
+newton = Math.floor(newton);
 
 console.log(`The temperature is ${kelvin} degrees Kelvin.
 The temperature is ${celsius} degrees Celsius.
-The temperature is ${fahrenheit} degrees Fahrenheit.`);
+The temperature is ${fahrenheit} degrees Fahrenheit.
+The temperature is ${newton} degrees Newton.`);
