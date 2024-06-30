0ea98448b70cd6253e27c1e9b29d35d5b1e3742d
hoodaddeveloper <mohammadkhaffas@gmail.com>
2024-06-30 14:16:33 +0200

    Convert Newton to Celsius

--- Bootcamp-Tutorials/tutorial-26.js
+++ Bootcamp-Tutorials/tutorial-26.js
@@ -1,14 +1,16 @@
 /// Weather converter ///
 
 // Kelvin temperature
-const kelvin = 293;
+const kelvin = 0;
 // Convert Kelvin to Celsius
 const celsius = kelvin - 273;
 // Convert Celsius to Fahrenheit
 let fahrenheit = celsius * (9 / 5) + 32;
 // Round down the Fahrenheit number
 fahrenheit = Math.floor(fahrenheit);
+// Convert Newton to Celsius
+let newton = celsius * (33 / 100);
 
-console.log(`The temperature is ${kelvin} degrees Kelvin`);
-console.log("The temperature is " + celsius + " degrees Celsius");
-console.log("The temperature is " + fahrenheit + " degrees Fahrenheit");
+console.log(`The temperature is ${kelvin} degrees Kelvin.
+The temperature is ${celsius} degrees Celsius.
+The temperature is ${fahrenheit} degrees Fahrenheit.`);
