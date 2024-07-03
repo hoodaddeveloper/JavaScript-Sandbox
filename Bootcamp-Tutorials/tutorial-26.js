dc9dc23ec2eec6b33a5822437fc785a6064d2f4e
hoodaddeveloper <mohammadkhaffas@gmail.com>
2024-07-03 23:05:41 +0200

    Comparison Operators

--- Bootcamp-Tutorials/tutorial-26.js
+++ Bootcamp-Tutorials/tutorial-26.js
@@ -91,3 +91,15 @@ if (sale) {
 } else {
   console.log("Time to wait for a sale.");
 }
+
+/// /// /// /// /// ///
+
+let hungerLevel = 7;
+
+if (hungerLevel > 7) {
+  console.log('Time to eat!');
+} else {
+  console.log('We can eat later!');
+}
+
+/// /// /// /// /// ///
