ce40e4eb9f552076c4fd17a82b45d29f32164912
hoodaddeveloper <mohammadkhaffas@gmail.com>
2024-07-03 23:46:56 +0200

    js true and false

--- Bootcamp-Tutorials/tutorial-26.js
+++ Bootcamp-Tutorials/tutorial-26.js
@@ -157,3 +157,19 @@ if (myVariable) {
 }
 
 /// /// /// /// /// ///
+
+let wordCount = 0; // or 1
+
+if (wordCount) {
+  console.log("Great! You've started your work!");
+} else {
+  console.log('Better get to work!');
+}
+
+let favoritePhrase = ''; // falsy string
+
+if (favoritePhrase) {
+  console.log("This string doesn't seem to be empty.");
+} else {
+  console.log('This string is definitely empty.');
+}
