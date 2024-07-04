c3cb5877201188d13189fb6f57176adc2a09298d
hoodaddeveloper <mohammadkhaffas@gmail.com>
2024-07-04 23:44:14 +0200

    The switch keyword

--- Bootcamp-Tutorials/tutorial-26.js
+++ Bootcamp-Tutorials/tutorial-26.js
@@ -248,3 +248,15 @@ if (season === 'spring') {
 }
 
 /// /// /// /// /// ///
+
+let groceryItem = 'papaya';
+
+if (groceryItem === 'tomato') {
+  console.log('Tomatoes are $0.49');
+} else if (groceryItem === 'papaya') {
+  console.log('Papayas are $1.29');
+} else {
+  console.log('Invalid item');
+}
+
+/// /// /// /// /// ///
