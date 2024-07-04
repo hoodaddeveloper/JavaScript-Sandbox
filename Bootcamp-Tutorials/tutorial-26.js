e06ab69dd07e443ce2f50c6dd1317235411238e3
hoodaddeveloper <mohammadkhaffas@gmail.com>
2024-07-04 23:44:54 +0200

    switch statement

--- Bootcamp-Tutorials/tutorial-26.js
+++ Bootcamp-Tutorials/tutorial-26.js
@@ -260,3 +260,25 @@ if (groceryItem === 'tomato') {
 }
 
 /// /// /// /// /// ///
+
+let groceryItem2 = 'papaya';
+
+switch (groceryItem2) {
+  case 'tomato':
+    console.log('Tomatoes are $0.49');
+    break;
+  case 'lime':
+    console.log('Limes are $1.49');
+    break;
+  case 'papaya':
+    console.log('Papayas are $1.29');
+    break;
+  default:
+    console.log('Invalid item');
+    break;
+}
+
+// Prints 'Papayas are $1.29'
+
+/// /// /// /// /// ///
+
