05fdf7c9dbed357ed44debe988a8264fed217f72
hoodaddeveloper <mohammadkhaffas@gmail.com>
2024-09-07 23:35:19 +0200

    practicing js

---
+++ Bahrami-Tutorials/t-1.js
@@ -0,0 +1,13 @@
+let myName = "Hoodad 'Wesker'"
+console.log(myName)
+
+let a = 2
+let b = 4
+let c = a ** b
+console.log(c)
+
+let d = 10
+console.log(d++)
+console.log(d)
+
+console.log("-----------------")
