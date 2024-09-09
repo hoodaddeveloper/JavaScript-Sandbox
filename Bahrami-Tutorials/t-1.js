0f9178ab4c9691f0104c684b6d1df0cf9c09257c
hoodaddeveloper <mohammadkhaffas@gmail.com>
2024-09-09 22:17:01 +0200

    switch case

--- Bahrami-Tutorials/t-1.js
+++ Bahrami-Tutorials/t-1.js
@@ -30,3 +30,34 @@ if (geld || zeit) {
 }
 
 console.log("-----------------")
+
+/* Switch Case */
+
+let day = 8
+
+switch (day) {
+  case 1:
+    console.log("Montag")
+    break;
+  case 2:
+    console.log("Dienstag")
+    break;
+  case 3:
+    console.log("Mittwoch")
+    break;
+  case 4:
+    console.log("Donnerstag")
+    break;
+  case 5:
+    console.log("Freitag")
+    break;
+  case 6:
+    console.log("Samstag")
+    break;
+  case 7:
+    console.log("Sonntag")
+    break;
+  default:
+    console.log("Fehler!")
+    break;
+}
