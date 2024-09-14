e48851eb7ec6f9d9574bf22cdfc08992db63cd40
hoodaddeveloper <mohammadkhaffas@gmail.com>
2024-09-14 23:51:14 +0200

    js

--- Bahrami-Tutorials/t-2.js
+++ Bahrami-Tutorials/t-2.js
@@ -3,7 +3,13 @@
 let array1 = ["Hoodad", 100, true]
 console.log(array1, array1[1])
 
+/* Change an index of the array */
 array1[0] = "Wesker"
 console.log(array1)
 
+/* Show the length of the array */
 console.log(array1.length)
+
+/* Add empty item into the array */
+array1.length = 4
+console.log(array1)
