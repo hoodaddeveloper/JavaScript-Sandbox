e5e2106ad384d0b4c7784665fb276ae7ef082921
hoodaddeveloper <mohammadkhaffas@gmail.com>
2024-12-07 22:26:23 +0100

    .

--- Bahrami-Tutorials/t-3.js
+++ Bahrami-Tutorials/t-3.js
@@ -1,4 +1,4 @@
-/* Learning JavaScript ES6 for REACT */
+/* Learning JavaScript ES6 for React */
 
 /* Ternary Operator */
 let age = 25;
@@ -7,5 +7,12 @@ age > 18 ? console.log("You are older than 18!") : console.log("You are under 18
 let result, average = 12.75;
 average > 12 ? result = "Pass!" : result = "Fail!"
 console.log(result)
+
+/// /// /// /// /// ///
+
+/* Backtick */
+let myName = "Hoodad"
+let myAge = 30
+console.log(`My name is ${myName} and I am ${myAge} years old.`)
 
 /// /// /// /// /// ///
