971beb3c8dbb60f4b5377f3fece612f1f2796adf
hoodaddeveloper <mohammadkhaffas@gmail.com>
2024-12-07 04:48:24 +0100

    Ternamy Operator

---
+++ Bahrami-Tutorials/t-3.js
@@ -0,0 +1,11 @@
+/* Learning JavaScript ES6 for REACT */
+
+/* Ternamy Operator */
+let age = 25;
+age > 18 ? console.log("You are older than 18!") : console.log("You are under 18!")
+
+let result, average = 12.75;
+average > 12 ? result = "Pass!" : result = "Fail!"
+console.log(result)
+
+/// /// /// /// /// ///
