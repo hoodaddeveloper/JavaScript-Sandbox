a8dd130eb23275522810e7e593145ece7ef9f2e7
hoodaddeveloper <mohammadkhaffas@gmail.com>
2024-12-09 03:09:52 +0100

    madules

---
+++ Bahrami-Tutorials/Madules/madule1.js
@@ -0,0 +1,8 @@
+let myName = "HoodadWesker";
+let myAge = 30;
+
+function coding() {
+  console.log("Hoodad is coding...");
+}
+
+export { myName, myAge, coding };
---
+++ Bahrami-Tutorials/Madules/madule2.js
@@ -0,0 +1,5 @@
+import { myName, myAge, coding } from "./madule1.js";
+
+console.log(myName);
+console.log(myAge);
+console.log(coding)
